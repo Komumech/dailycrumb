2b66abbb71c0a913c16302c16545700c3aa6bb59
Komumech <komumech@gmail.com>
2025-08-23 04:02:35 +0000

    Updated Changes

--- script.js
+++ script.js
@@ -127,7 +127,7 @@ gsap.fromTo(
                     gsap.to(".logo-img", { backgroundColor: "rgba(217, 164, 65, 0)", duration: 0.2, overwrite: "auto" });
                 } else {
                     // Colored for the rest of the scroll
-                    gsap.to(".logo-img", { backgroundColor: "#f8f4edbe", duration: 5, overwrite: "auto", boxShadow: #f8f4ed });
+                    gsap.to(".logo-img", { backgroundColor: "#f8f4edbe", duration: , overwrite: "auto" });
                 }
             }
         }
