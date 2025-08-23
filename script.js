693ca0c5b9a872a48a9cc5da634bfcf3d33cb5d1
Komumech <komumech@gmail.com>
2025-08-23 04:06:17 +0000

    mnj

--- script.js
+++ script.js
@@ -130,7 +130,7 @@ gsap.fromTo(
                 gsap.to(".logo-img", {
   backgroundColor: "#f8f4ed8d",
   duration: 0.2,
-  boxShadow: "0px 0px 12px #f8f4ed",
+  boxShadow: "0px 0px 12px #f8f4ed07",
   overwrite: "auto"
 });
 
