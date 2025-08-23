f62ae93c0a77c60cc48edafc8ab4391f33b066a9
Komumech <komumech@gmail.com>
2025-08-23 04:05:15 +0000

    nn

--- script.js
+++ script.js
@@ -128,9 +128,9 @@ gsap.fromTo(
                 } else {
                     // Colored for the rest of the scroll
                 gsap.to(".logo-img", {
-  backgroundColor: "#f8f4ed",
+  backgroundColor: "#f8f4ed8d",
   duration: 0.2,
-  boxShadow: "0px 4px 12px #f8f4ed",
+  boxShadow: "0px 0px 12px #f8f4ed",
   overwrite: "auto"
 });
 
