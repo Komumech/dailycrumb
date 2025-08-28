fa23cfe2d2b9b28bfe51895ad655ebd5a2585579
Komumech <komumech@gmail.com>
2025-08-28 01:57:15 +0000

    Updated Changes

--- menu.js
+++ menu.js
@@ -45,3 +45,93 @@ function RosemarySourdough(){
     document.querySelector(".order").innerHTML = displayOrder;
     document.querySelector(".price").innerHTML = "N "+ price;
 }
+function MultigrainSourdough(){
+    price += 5500;
+    order.push("Olive & Rosemary Sourdough");
+    document.querySelector(".order").innerHTML = order.join(", ");
+    console.log(price);
+    const counts = {};
+    order.forEach(item => {
+        counts[item] = (counts[item] || 0) + 1;
+    });
+    const displayOrder = Object.entries(counts)
+        .map(([item, count]) => count > 1 ? `${item} (${count})` : item)
+        .join(", ");
+    document.querySelector(".order").innerHTML = displayOrder;
+    document.querySelector(".price").innerHTML = "N "+ price;
+}
+function RosemarySourdough(){
+    price += 5500;
+    order.push("Olive & Rosemary Sourdough");
+    document.querySelector(".order").innerHTML = order.join(", ");
+    console.log(price);
+    const counts = {};
+    order.forEach(item => {
+        counts[item] = (counts[item] || 0) + 1;
+    });
+    const displayOrder = Object.entries(counts)
+        .map(([item, count]) => count > 1 ? `${item} (${count})` : item)
+        .join(", ");
+    document.querySelector(".order").innerHTML = displayOrder;
+    document.querySelector(".price").innerHTML = "N "+ price;
+}
+function RosemarySourdough(){
+    price += 5500;
+    order.push("Olive & Rosemary Sourdough");
+    document.querySelector(".order").innerHTML = order.join(", ");
+    console.log(price);
+    const counts = {};
+    order.forEach(item => {
+        counts[item] = (counts[item] || 0) + 1;
+    });
+    const displayOrder = Object.entries(counts)
+        .map(([item, count]) => count > 1 ? `${item} (${count})` : item)
+        .join(", ");
+    document.querySelector(".order").innerHTML = displayOrder;
+    document.querySelector(".price").innerHTML = "N "+ price;
+}
+function RosemarySourdough(){
+    price += 5500;
+    order.push("Olive & Rosemary Sourdough");
+    document.querySelector(".order").innerHTML = order.join(", ");
+    console.log(price);
+    const counts = {};
+    order.forEach(item => {
+        counts[item] = (counts[item] || 0) + 1;
+    });
+    const displayOrder = Object.entries(counts)
+        .map(([item, count]) => count > 1 ? `${item} (${count})` : item)
+        .join(", ");
+    document.querySelector(".order").innerHTML = displayOrder;
+    document.querySelector(".price").innerHTML = "N "+ price;
+}
+function RosemarySourdough(){
+    price += 5500;
+    order.push("Olive & Rosemary Sourdough");
+    document.querySelector(".order").innerHTML = order.join(", ");
+    console.log(price);
+    const counts = {};
+    order.forEach(item => {
+        counts[item] = (counts[item] || 0) + 1;
+    });
+    const displayOrder = Object.entries(counts)
+        .map(([item, count]) => count > 1 ? `${item} (${count})` : item)
+        .join(", ");
+    document.querySelector(".order").innerHTML = displayOrder;
+    document.querySelector(".price").innerHTML = "N "+ price;
+}
+function RosemarySourdough(){
+    price += 5500;
+    order.push("Olive & Rosemary Sourdough");
+    document.querySelector(".order").innerHTML = order.join(", ");
+    console.log(price);
+    const counts = {};
+    order.forEach(item => {
+        counts[item] = (counts[item] || 0) + 1;
+    });
+    const displayOrder = Object.entries(counts)
+        .map(([item, count]) => count > 1 ? `${item} (${count})` : item)
+        .join(", ");
+    document.querySelector(".order").innerHTML = displayOrder;
+    document.querySelector(".price").innerHTML = "N "+ price;
+}
