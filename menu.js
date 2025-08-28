178075e866ec49aa92cabd855131b068a11eff03
Komumech <komumech@gmail.com>
2025-08-28 00:59:15 +0000

    Updated Changes

--- menu.js
+++ menu.js
@@ -2,6 +2,8 @@ var price = 0;
 var order= [];
 function clickCinnamon(){
     price += 700;
+    order.push("Cinnamon Roll");
+    document.querySelector(".order").innerHTML = order.join(", ");
     console.log(price);
     document.querySelector(".price").innerHTML = "N "+ price;
 }
