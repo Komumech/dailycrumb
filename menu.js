e223bbf679e88583125d903693a04120f363a960
Komumech <komumech@gmail.com>
2025-08-28 00:57:15 +0000

    Updated Changes

--- menu.js
+++ menu.js
@@ -2,5 +2,5 @@ var price = 0;
 function clickCinnamon(){
     price += 700;
     console.log(price);
-    document.querySelector(".price").innerHTML = price;
+    document.querySelector(".price").innerHTML = "N "+ price;
 }
